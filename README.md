632ade6b31eb7009fbb0e7e4b68fb6fdce093d3c
Vinicius Giovanini <vgiovanini@sga.pucminas.br>
2024-04-26 22:14:50 -0300

    feat: Transformer keras basico funcionando sem pesos congelados

--- README.md
+++ README.md
@@ -23,6 +23,39 @@ env/Scripts/activated
 pip install -r requirements.txt
 ```
 
+## Rodando Codigo do Tensorflow na GPU
+
+- No WSL2 em uma distro linux baixe o tensorflow cuda.
+
+```
+pip install tensorflow[and-cuda]
+```
+
+- Verifique a versão do Cuda instalada por default no WSL2
+```
+nvidia-smi
+watch -n 1 nvidia-smi
+```
+
+- Caso seja preciso instalar outra versão do Cuda por cima, tem que verificar pelo seguinte comando
+```
+nvcc -V
+```
+
+- Na versão 2.16.1 (26/04/2024) ele está bugada não reconhecendo a GPU, porém é a unica versão possivel de rodar transformer, então rode os seguintes codigos.
+
+```
+export NVIDIA_DIR=$(dirname $(dirname $(python3 -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)")))
+export LD_LIBRARY_PATH=$(echo ${NVIDIA_DIR}/*/lib/ | sed -r 's/\s+/:/g')${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+```
+
+- Use o código abaixo para verificar se retorna a GPU da nvidia.
+
+```
+python3 -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
+```
+
+
 ## Linguagens de Desenvolvimento
 
 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" width="50px"/>&nbsp;
---
+++ VIT_keras_gpu.py
@@ -0,0 +1,274 @@
+import keras
+from keras import ops
+from keras import layers
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
+import math
+import tensorflow as tf
+
+print("\n\n\n")
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+print("\n\n\n")
+
+# Hyperparameters
+
+learning_rate = 0.001
+weight_decay = 0.0001
+batch_size = 32
+num_epochs = 200  # For real training, use num_epochs=100. 10 is a test value
+image_size = 200  # We'll resize input images to this size
+patch_size = 16  # Size of the patches to be extract from the input images
+num_patches = (image_size // patch_size) ** 2
+projection_dim = 64
+num_heads = 4
+transformer_units = [
+    projection_dim * 2,
+    projection_dim,
+]  # Size of the transformer layers
+transformer_layers = 8
+mlp_head_units = [
+    2048,
+    1024,
+]  # Size of the dense layers of the final classifier
+
+# Reading Data
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_dir = '../VIT-dog-cat/data/dataset_treino_e_teste/train/'
+test_dir = '../VIT-dog-cat/data/dataset_treino_e_teste/test/'
+
+input_shape = (image_size, image_size, 3)
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(input_shape[0], input_shape[1]),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+test_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(input_shape[0], input_shape[1]),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+print("Classes de treino:", train_generator.class_indices)
+print("Classes de teste:", test_generator.class_indices)
+
+
+num_classes = len(train_generator.class_indices)
+
+# Transform data
+
+x_train = []
+y_train = []
+x_test = []
+y_test = []
+
+for i in range(len(train_generator)):
+    x_batch, y_batch = train_generator[i]
+    x_train.append(x_batch)
+    y_train.append(y_batch)
+
+x_train = np.concatenate(x_train)
+y_train = np.concatenate(y_train)
+
+for i in range(len(test_generator)):
+    x_batch, y_batch = test_generator[i]
+    x_test.append(x_batch)
+    y_test.append(y_batch)
+
+x_test = np.concatenate(x_test)
+y_test = np.concatenate(y_test)
+
+print(f"Shape x_train {x_train.shape}")
+print(f"Shape y_train {y_train.shape}")
+print(f"Shape x_test {x_test.shape}")
+print(f"Shape y_test {y_test.shape}")
+
+# Data augmentation
+
+data_augmentation = keras.Sequential(
+    [
+        layers.Normalization(),
+        layers.Resizing(image_size, image_size),
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(factor=0.02),
+        layers.RandomZoom(height_factor=0.2, width_factor=0.2),
+    ],
+    name="data_augmentation",
+)
+# Compute the mean and the variance of the training data for normalization.
+data_augmentation.layers[0].adapt(x_train)
+data_augmentation
+
+# MLP (Multilayer Perceptron)
+
+def mlp(x, hidden_units, dropout_rate):
+    for units in hidden_units:
+        x = layers.Dense(units, activation=keras.activations.gelu)(x)
+        x = layers.Dropout(dropout_rate)(x)
+    return x
+  
+# Patch creation
+
+class Patches(layers.Layer):
+    def __init__(self, patch_size):
+        super().__init__()
+        self.patch_size = patch_size
+
+    def call(self, images):
+        input_shape = ops.shape(images)
+        batch_size = input_shape[0]
+        height = input_shape[1]
+        width = input_shape[2]
+        channels = input_shape[3]
+        num_patches_h = height // self.patch_size
+        num_patches_w = width // self.patch_size
+        patches = keras.ops.image.extract_patches(images, size=self.patch_size)
+        patches = ops.reshape(
+            patches,
+            (
+                batch_size,
+                num_patches_h * num_patches_w,
+                self.patch_size * self.patch_size * channels,
+            ),
+        )
+        return patches
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"patch_size": self.patch_size})
+        return config
+      
+# Patch encoding layer
+
+class PatchEncoder(layers.Layer):
+    def __init__(self, num_patches, projection_dim):
+        super().__init__()
+        self.num_patches = num_patches
+        self.projection = layers.Dense(units=projection_dim)
+        self.position_embedding = layers.Embedding(
+            input_dim=num_patches, output_dim=projection_dim
+        )
+
+    def call(self, patch):
+        positions = ops.expand_dims(
+            ops.arange(start=0, stop=self.num_patches, step=1), axis=0
+        )
+        projected_patches = self.projection(patch)
+        encoded = projected_patches + self.position_embedding(positions)
+        return encoded
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"num_patches": self.num_patches})
+        return config
+      
+# Build VIT Model
+def create_vit_classifier():
+    inputs = keras.Input(shape=input_shape)
+    # Augment data.
+    augmented = data_augmentation(inputs)
+    # Create patches.
+    patches = Patches(patch_size)(augmented)
+    # Encode patches.
+    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
+
+    # Create multiple layers of the Transformer block.
+    for _ in range(transformer_layers):
+        # Layer normalization 1.
+        x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+        # Create a multi-head attention layer.
+        attention_output = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+        )(x1, x1)
+        # Skip connection 1.
+        x2 = layers.Add()([attention_output, encoded_patches])
+        # Layer normalization 2.
+        x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+        # MLP.
+        x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
+        # Skip connection 2.
+        encoded_patches = layers.Add()([x3, x2])
+
+       
+    # Create a [batch_size, projection_dim] tensor.
+    representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+    representation = layers.Flatten()(representation)
+    representation = layers.Dropout(0.5)(representation)
+    # Add MLP.
+    features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.5)
+    # Classify outputs.
+    # logits = layers.Dense(num_classes)(features)
+    
+    # Classify outputs.
+    logits = layers.Dense(2, activation='sigmoid')(features)
+    # Create the Keras model.
+    model = keras.Model(inputs=inputs, outputs=logits)
+    return model
+  
+
+# Run Model
+
+def run_experiment(model):
+    optimizer = keras.optimizers.AdamW(
+        learning_rate=learning_rate, weight_decay=weight_decay
+    )
+
+    model.compile(
+    optimizer=optimizer,
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[
+        keras.metrics.BinaryAccuracy(name="accuracy"),
+      ],
+    )
+
+
+    checkpoint_filepath = "./models/checkpoint.GPU.weights.h5"
+    checkpoint_callback = keras.callbacks.ModelCheckpoint(
+        checkpoint_filepath,
+        monitor="val_accuracy",
+        save_best_only=True,
+        save_weights_only=True,
+    )
+    
+    history = model.fit(
+        x=x_train,
+        y=y_train,
+        batch_size=batch_size,
+        epochs=num_epochs,
+        validation_split=0.1,
+        callbacks=[checkpoint_callback],
+    )
+
+    
+    model.load_weights(checkpoint_filepath)
+    _, accuracy = model.evaluate(x_test, y_test)
+    print(f"Test accuracy: {round(accuracy * 100, 2)}%")
+
+    return history
+
+
+vit_classifier = create_vit_classifier()
+history = run_experiment(vit_classifier)
+
+
+def plot_history(item):
+    plt.plot(history.history[item], label=item)
+    plt.plot(history.history["val_" + item], label="val_" + item)
+    plt.xlabel("Epochs")
+    plt.ylabel(item)
+    plt.title("Train and Validation {} Over Epochs".format(item), fontsize=14)
+    plt.legend()
+    plt.grid()
+    plt.savefig(f'{item}.svg')
+
+plot_history("loss")
+plot_history("accuracy")
+
